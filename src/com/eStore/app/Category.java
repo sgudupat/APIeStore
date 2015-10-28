e47c3213823d1109e271db01cbdbf5a4862bbb51
davidson <davidson0790@gmail.com>
2015-10-28 19:26:39 +0530

    Added G+ Login and Background Activities also Category Related Xml Files Adapter Classes

---
+++ src/com/eStore/app/Category.java
@@ -0,0 +1,30 @@
+package com.eStore.app;
+
+public class Category {
+	String categoryName;
+	String categoryUrl;
+	Category(String categoryName, String categoryUrl){
+		this.categoryName=categoryName;
+		this.categoryUrl=categoryUrl;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	public String getCategoryUrl() {
+		return categoryUrl;
+	}
+	public void setCategoryUrl(String categoryUrl) {
+		this.categoryUrl = categoryUrl;
+	}
+	@Override
+	public String toString() {
+		return "Category [categoryName=" + categoryName + ", categoryUrl="
+				+ categoryUrl + "]";
+	}
+	
+	
+
+}
---
+++ src/com/eStore/app/CategoryAdapter.java
@@ -0,0 +1,60 @@
+package com.eStore.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+public class CategoryAdapter extends BaseAdapter implements ListAdapter {
+
+	private List<Category> list = new ArrayList<Category>();
+	private Context context;
+	 CategoryAdapter (List<Category> list, Context context){
+		this.list=list;
+		this.context=context;
+	}
+	@Override
+	public int getCount() {
+		Log.i("category count", "Category Count");
+		Log.i(" count", ""+list.size());
+		return list.size();
+		
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return list.get(position);
+	}
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View view = convertView;
+		if(view == null){
+			LayoutInflater inflater= (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+			view= inflater.inflate(R.layout.category_item, null);
+		
+		}
+		TextView catName = (TextView) view.findViewById(R.id.categoryName);
+		TextView catUrl = (TextView) view.findViewById(R.id.category_url);
+		catName.setText(list.get(position).getCategoryName());
+		catUrl.setText(list.get(position).getCategoryUrl());
+		return view;
+	}
+	
+}
