cf667906b7a5c0b1fa3f1647708f4781d7767fa2
davidson <davidson0790@gmail.com>
2015-11-16 14:25:18 +0530

    G + Login Removed

--- src/com/eStore/app/HomeActivity.java
+++ src/com/eStore/app/HomeActivity.java
@@ -1,64 +1,93 @@
 package com.eStore.app;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.os.Handler;
-import android.preference.PreferenceManager;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.*;
-import com.eStore.app.common.SimpleHttpClient;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.List;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.IntentSender.SendIntentException;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class HomeActivity extends Activity implements OnClickListener, ConnectionCallbacks, OnConnectionFailedListener {
-    private static final int RC_SIGN_IN = 0;
-    // Logcat tag
-    private static final String TAG = "MainActivity";
 
-    // Profile pic image size in pixels
-    private static final int PROFILE_PIC_SIZE = 400;
 
-    // Google client to interact with Google API
-    private GoogleApiClient mGoogleApiClient;
+import com.eStore.app.common.SimpleHttpClient;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
 
-    /**
-     * A flag indicating that a PendingIntent is in progress and prevents us
-     * from starting further intents.
-     */
-    private boolean mIntentInProgress;
 
-    private boolean mSignInClicked;
-
-    private ConnectionResult mConnectionResult;
-
-    private SignInButton btnSignIn;
-    private Button btnSignOut, btnRevokeAccess;
-    private ImageView imgProfilePic;
-    private TextView txtName, txtEmail;
-    private LinearLayout llProfileLayout;
-    final Context context = this;
-    // private Context context;
-    ArrayList<Category> items = new ArrayList<Category>();
-    int clientId = 2;
-    int images[];
-    LinearLayout linearLayout;
-    public static int count = 0;
-    int[] drawablearray = new int[]{R.drawable.bg, R.drawable.image6, R.drawable.image1, R.drawable.image2, R.drawable.image3, R.drawable.image4, R.drawable.image5, R.drawable.image16, R.drawable.image7, R.drawable.image8, R.drawable.image9, R.drawable.image10, R.drawable.image11, R.drawable.image12, R.drawable.image13, R.drawable.image14};
-
+public class HomeActivity extends Activity implements OnClickListener,
+ConnectionCallbacks, OnConnectionFailedListener {
+	 private static final int RC_SIGN_IN = 0;
+	    // Logcat tag
+	    private static final String TAG = "MainActivity";
+	 
+	    // Profile pic image size in pixels
+	    private static final int PROFILE_PIC_SIZE = 400;
+	 
+	    // Google client to interact with Google API
+	    private GoogleApiClient mGoogleApiClient;
+	 
+	    /**
+	     * A flag indicating that a PendingIntent is in progress and prevents us
+	     * from starting further intents.
+	     */
+	    private boolean mIntentInProgress;
+	 
+	    private boolean mSignInClicked;
+	 
+	    private ConnectionResult mConnectionResult;
+	 
+	    private SignInButton btnSignIn;
+	    private Button btnSignOut, btnRevokeAccess;
+	    private ImageView imgProfilePic;
+	    private TextView txtName, txtEmail;
+	    private LinearLayout llProfileLayout;
+	final Context context = this;
+	   // private Context context;
+		ArrayList<Category> items = new ArrayList<Category>();
+		int clientId=2;
+	 int images[];
+	LinearLayout linearLayout;
+	public static int count=0;
+	int[] drawablearray=new int[]{R.drawable.bg,R.drawable.image6,R.drawable.image1,R.drawable.image2,R.drawable.image3,R.drawable.image4,R.drawable.image5,R.drawable.image16,R.drawable.image7,R.drawable.image8,R.drawable.image9,R.drawable.image10,R.drawable.image11,R.drawable.image12,R.drawable.image13,R.drawable.image14};
     /**
      * Called when the activity is first created.
      */
@@ -66,25 +95,31 @@ public class HomeActivity extends Activity implements OnClickListener, Connectio
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+   
+    	/*ListView listView = (ListView) findViewById(R.id.categoryListview);
+			// Adding items to list view
+			// 1. pass context and data to the custom adapter        
+			final CategoryAdapter adapter = new CategoryAdapter(generateData(clientId), this);
+			Log.i("After adapter excecution", "excecution");
+			listView.setAdapter(adapter);*/
+        
+     new Handler().postDelayed(new Runnable() {
+         public void run() {
 
+            if(count<drawablearray.length){
 
-        new Handler().postDelayed(new Runnable() {
-            public void run() {
+            	HomeActivity.this.getWindow().
+                    setBackgroundDrawableResource(drawablearray[count]);
+                count++;  //<<< increment counter here
+             }
+            else{
+               // reset counter here
+               count=0;
+             }
 
-                if (count < drawablearray.length) {
-
-                    HomeActivity.this.getWindow().
-                            setBackgroundDrawableResource(drawablearray[count]);
-                    count++;  //<<< increment counter here
-                } else {
-                    // reset counter here
-                    count = 0;
-                }
-
-            }
-        }, 0);
-    }
-   /*  btnSignIn = (SignInButton) findViewById(R.id.btn_sign_in);
+           }
+       }, 0);}
+    /* btnSignIn = (SignInButton) findViewById(R.id.btn_sign_in);
      btnSignOut = (Button) findViewById(R.id.btn_sign_out);
      btnRevokeAccess = (Button) findViewById(R.id.btn_revoke_access);
      imgProfilePic = (ImageView) findViewById(R.id.imgProfilePic);
@@ -116,8 +151,8 @@ public class HomeActivity extends Activity implements OnClickListener, Connectio
  }
 
  *//**
-     * Method to resolve any signin errors
-     * *//*
+  * Method to resolve any signin errors
+  * *//*
  private void resolveSignInError() {
      if (mConnectionResult.hasResolution()) {
          try {
@@ -175,8 +210,6 @@ public class HomeActivity extends Activity implements OnClickListener, Connectio
 
      // Get user's information
      getProfileInformation();
-     Intent intent=new Intent(this,CategoryActivity.class);
-     startActivity(intent);
 
      // Update the UI after signin
      updateUI(true);
@@ -185,8 +218,8 @@ public class HomeActivity extends Activity implements OnClickListener, Connectio
  
 
  *//**
-     * Updating the UI, showing/hiding buttons and profile layout
-     * *//*
+  * Updating the UI, showing/hiding buttons and profile layout
+  * *//*
  private void updateUI(boolean isSignedIn) {
      if (isSignedIn) {
          btnSignIn.setVisibility(View.GONE);
@@ -202,8 +235,8 @@ public class HomeActivity extends Activity implements OnClickListener, Connectio
  }
 
  *//**
-     * Fetching user's information name, email, profile pic
-     * *//*
+  * Fetching user's information name, email, profile pic
+  * *//*
  private void getProfileInformation() {
      try {
          if (Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null) {
@@ -248,8 +281,8 @@ public class HomeActivity extends Activity implements OnClickListener, Connectio
 
 
  *//**
-     * Button on click listener
-     * *//*
+  * Button on click listener
+  * *//*
  @Override
  public void onClick(View v) {
      switch (v.getId()) {
@@ -269,8 +302,8 @@ public class HomeActivity extends Activity implements OnClickListener, Connectio
  }
 
  *//**
-     * Sign-in into google
-     * *//*
+  * Sign-in into google
+  * *//*
  private void signInWithGplus() {
      if (!mGoogleApiClient.isConnecting()) {
          mSignInClicked = true;
@@ -279,8 +312,8 @@ public class HomeActivity extends Activity implements OnClickListener, Connectio
  }
 
  *//**
-     * Sign-out from google
-     * *//*
+  * Sign-out from google
+  * *//*
  private void signOutFromGplus() {
      if (mGoogleApiClient.isConnected()) {
          Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
@@ -291,8 +324,8 @@ public class HomeActivity extends Activity implements OnClickListener, Connectio
  }
 
  *//**
-     * Revoking access from google
-     * *//*
+  * Revoking access from google
+  * *//*
  private void revokeGplusAccess() {
      if (mGoogleApiClient.isConnected()) {
          Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
@@ -309,11 +342,9 @@ public class HomeActivity extends Activity implements OnClickListener, Connectio
      }
  }
 
- */
-
-    /**
-     * Background Async task to load user profile picture from url
-     *//*
+ *//**
+  * Background Async task to load user profile picture from url
+  * *//*
  private class LoadProfileImage extends AsyncTask<String, Void, Bitmap> {
      ImageView bmImage;
 
@@ -338,74 +369,85 @@ public class HomeActivity extends Activity implements OnClickListener, Connectio
          bmImage.setImageBitmap(result);
      }
  }
-     */
-    private List<Category> generateData(int clientId2) {
-        // TODO Auto-generated method stub
-        String cName = "Sachin";
-        String cUrl = "http://www.androidhive.info/2011/08/android-tab-layout-tutorial/";
-        items.add(new Category(cName, cUrl));
-        return items;
-    }
+    */ 
+
+ 
+private List<Category> generateData(int clientId2) {
+		// TODO Auto-generated method stub
+		String cName= "Sachin";
+		String cUrl="http://www.androidhive.info/2011/08/android-tab-layout-tutorial/";
+		items.add(new Category(cName,cUrl));
+		return items;
+	}
 
 
-    public void signinEstore(View view) {
-        final EditText userName = (EditText) findViewById(R.id.editText1);
 
-        final EditText password = (EditText) findViewById(R.id.editText2);
-        try {
-            final ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
-            postParameters.add(new BasicNameValuePair("username", userName.getText().toString()));
-            postParameters.add(new BasicNameValuePair("password", password.getText().toString()));
-            String response = SimpleHttpClient.executeHttpPost("/loginUser", postParameters);
+/*private int getRandomNumber() {
+	Log.i("getRandomNumber", "getRandomNumber");
+    //Note that general syntax is Random().nextInt(n)
+    //It results in range 0-4
+    //So it should be equal to number of images in images[] array
+    return new Random().nextInt(3);
+}*/
 
+    public void signinEstore(View view){
+    	final EditText userName = (EditText) findViewById(R.id.editText1);
+	
+	 	final EditText password = (EditText) findViewById(R.id.editText2);
+	 	 try {
+             final ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+             postParameters.add(new BasicNameValuePair("username", userName.getText().toString()));
+             postParameters.add(new BasicNameValuePair("password", password.getText().toString()));
+             String response = SimpleHttpClient.executeHttpPost("/loginUser", postParameters);
+             Log.i("Response:", response);
             JSONObject jsonobject = new JSONObject(response);
-            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-            SharedPreferences.Editor editor = prefs.edit();
-            editor.putString("username", (String) jsonobject.get("user_name"));
-            editor.putString("email", (String) jsonobject.get("email"));
-            editor.putString("mobile", (String) jsonobject.get("mobile"));
-            editor.commit();
-            Intent intent = new Intent(this, CategoryActivity.class);
-            startActivity(intent);
-        } catch (Exception e) {
-
-            Toast.makeText(getApplicationContext(), "Login Failed, Please Retry !!!", Toast.LENGTH_LONG).show();
-        }
-
+             SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+             SharedPreferences.Editor editor = prefs.edit();
+             Log.i("username", (String) jsonobject.get("user_name"));
+             editor.putString("username", (String) jsonobject.get("user_name"));
+             editor.putString("email", (String) jsonobject.get("email"));
+             editor.putString("mobile", (String) jsonobject.get("mobile"));
+             editor.commit();
+             Intent intent= new Intent(this, StoreActivity.class);
+    	startActivity(intent);
+         } catch (Exception e) {
+             Log.e("register", e.getMessage() + "");
+             Toast.makeText(getApplicationContext(), "Login Failed, Please Retry !!!", Toast.LENGTH_LONG).show();
+         }
+    	
     }
-
-    public void EstoreRegister(View view) {
-        Intent intent = new Intent(this, RegisterActivity.class);
-        startActivity(intent);
+    public void EstoreRegister(View view){
+    	Intent intent = new Intent(this, RegisterActivity.class);
+    	startActivity(intent);
     }
-
-    public void EstoreForgotPassword(View view) {
-        Intent intent = new Intent(this, ForgotPasswordActivity.class);
-        startActivity(intent);
+    public void EstoreForgotPassword(View view){
+    	Intent intent =new Intent(this, ForgotPasswordActivity.class);
+    	startActivity(intent);
     }
+	@Override
+	public void onConnectionFailed(ConnectionResult arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void onConnected(Bundle arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void onConnectionSuspended(int arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
 
-    @Override
-    public void onConnectionFailed(ConnectionResult arg0) {
-        // TODO Auto-generated method stub
 
-    }
 
-    @Override
-    public void onConnected(Bundle arg0) {
-        // TODO Auto-generated method stub
 
-    }
-
-    @Override
-    public void onConnectionSuspended(int arg0) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void onClick(View v) {
-        // TODO Auto-generated method stub
-
-    }
+	
 
 }
