9746e85631178d4eb7c584dec38e72e04e8cac02
sgudupat <sandeep.g@pscglobalsolutions.com>
2015-11-19 19:30:05 +0530

    Fixed error and category list

--- src/com/getshared/eStore/app/ProductListAdapter.java
+++ src/com/getshared/eStore/app/ProductListAdapter.java
@@ -1,7 +1,5 @@
 package com.getshared.eStore.app;
 
-import java.util.ArrayList;
-
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -14,102 +12,101 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.getshared.eStore.domain.Product;
+
+import java.util.ArrayList;
 
 
 public class ProductListAdapter extends BaseAdapter {
-	private Context context;
-	ArrayList<Product> dataList;
-	ImageView view;
+    private Context context;
+    ArrayList<Product> dataList;
+    ImageView view;
 
-	public ProductListAdapter(Context context, ArrayList<Product> oslist) {
-	    this.context = context;
-	    this.dataList = oslist;
-	}
+    public ProductListAdapter(Context context, ArrayList<Product> oslist) {
+        this.context = context;
+        this.dataList = oslist;
+    }
 
-	public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View convertView, ViewGroup parent) {
 
-	    LayoutInflater inflater = (LayoutInflater) context
-	            .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-	    View gridView;
-	
+        View gridView;
 
-	    if (convertView == null) {
-Log.i("adapter", "adapter");
-	        gridView = new View(context);
 
-	        // get layout from mobile.xml
-	        gridView = inflater.inflate(R.layout.adapter_second, null);
+        if (convertView == null) {
+            Log.i("adapter", "adapter");
+            gridView = new View(context);
 
-	 
+            // get layout from mobile.xml
+            gridView = inflater.inflate(R.layout.adapter_second, null);
 
-	    } else {
-	        gridView = (View) convertView;
-	    }
-	       // set value into textview
-        TextView textPrice = (TextView) gridView
-                .findViewById(R.id.company);
+
+        } else {
+            gridView = (View) convertView;
+        }
+        // set value into textview
+        TextView textPrice = (TextView) gridView.findViewById(R.id.company);
         textPrice.setText(dataList.get(position).getPrice());
-    	        TextView textcompany = (TextView) gridView
-    	                .findViewById(R.id.company_add);
-    	    	        textcompany.setText(dataList.get(position).getpCompany());
+        TextView textcompany = (TextView) gridView.findViewById(R.id.add);
+        textcompany.setText(dataList.get(position).getpCompany());
         TextView textView = (TextView) gridView
                 .findViewById(R.id.decline);
-        TextView title=(TextView) gridView.findViewById(R.id.name);
+        TextView title = (TextView) gridView.findViewById(R.id.name);
         title.setText(dataList.get(position).getName());
-       // textView.setText(dataList.get(position).getName());
+        // textView.setText(dataList.get(position).getName());
         // set image based on selected text
         ImageView imageView = (ImageView) gridView
                 .findViewById(R.id.photo);
-        
+
         textView.setOnClickListener(new LinkProduct(
                 dataList.get(position).getLink()));
         SpannableString content = new SpannableString(dataList.get(position).getLink());
         content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
-        
-       
-       {
+
+
+        {
             imageView.setImageBitmap(dataList.get(position).getTransformedImage());
-    		
+
         }
 
-	    return gridView;
-	}
+        return gridView;
+    }
 
-	@Override
-	public int getCount() {
-	    return dataList.size();
-	}
+    @Override
+    public int getCount() {
+        return dataList.size();
+    }
 
-	@Override
-	public Product getItem(int position) {
-	    return dataList.get(position);
-	}
+    @Override
+    public Product getItem(int position) {
+        return dataList.get(position);
+    }
 
-	@Override
-	public long getItemId(int position) {
-	    return 0;
-	}
-	public class LinkProduct implements View.OnClickListener {
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    public class LinkProduct implements View.OnClickListener {
         private String link;
-      
 
-        public LinkProduct (String link) {
-            this.link= link;
-            
+
+        public LinkProduct(String link) {
+            this.link = link;
+
         }
 
         @Override
         public void onClick(View v) {
-        	Log.i("ProductLink", link);
-          Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                intent.setData(Uri.parse(link));
-                context.startActivity(intent);
+            Log.i("ProductLink", link);
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.addCategory(Intent.CATEGORY_BROWSABLE);
+            intent.setData(Uri.parse(link));
+            context.startActivity(intent);
         }
     }
 
-	}
+}
