5b8588b17ea7a24d5f64bef5323c57d7a7ee054a
davidson <davidson0790@gmail.com>
2015-12-09 18:34:58 +0530

    Added  Fonts in style.xml

--- src/com/getshared/eStore/app/HomeScreenActivity.java
+++ src/com/getshared/eStore/app/HomeScreenActivity.java
@@ -4,34 +4,64 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class HomeScreenActivity extends Activity {
-
+	final Context context= this;
+	//private Context context;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home_screen);
+		isNetworkAvailable(context);
 		int delay = 3000; // delay for 1 sec. 		
 		Timer timer = new Timer(); 
-		
+
 		timer.schedule(new TimerTask() 
-		    { 
-		        public void run() 
-		        { 
-		        	goToNext();  // display the data
-		        } 
-		    }, delay); 
-		
+		{ 
+			public void run() 
+			{ 
+
+				
+				goToNext();  // display the data
+			} 
+		}, delay); 
+
 	}
 
 	private void goToNext() {
 		// TODO Auto-generated method stub
-		
+
 		Intent intent = new Intent(this, CategoryActivity.class);
 		startActivity(intent);   
 		finish();
-	
+
 	}
-}
+	private boolean isNetworkAvailable(Context context) {
+		Log.i("network connection", "network connectivity");
+		ConnectivityManager connectivity = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+		try{
+		if (connectivity != null) {
+			NetworkInfo[] info = connectivity.getAllNetworkInfo();
+			if (info != null) {
+				for (int i = 0; i < info.length; i++) {
+					Log.w("INTERNET:",String.valueOf(i));
+					if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+						Log.w("INTERNET:", "connected!");    
+						Toast.makeText(getApplicationContext(), "NETWORK  CONNECTED !!!", Toast.LENGTH_LONG).show();
+						return true;
+					}}
+				}
+			}
+		}catch (Exception e) {
+			Log.e("register", e.getMessage() + "");
+			Toast.makeText(getApplicationContext(), "No Network Connectivity, Please check again!!!", Toast.LENGTH_LONG).show();
+	}
+		return false;
+}}
