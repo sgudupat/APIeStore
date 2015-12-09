3d6c900309233904c2c8705cad68ba555739ef6f
davidson <davidson0790@gmail.com>
2015-12-09 14:05:44 +0530

    Some Modification in HomeScreenActivity

--- src/com/getshared/eStore/app/HomeScreenActivity.java
+++ src/com/getshared/eStore/app/HomeScreenActivity.java
@@ -1,20 +1,37 @@
 package com.getshared.eStore.app;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
 public class HomeScreenActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_screen);
-        try {
-            Thread.sleep(3000);
-            Intent intent = new Intent(this, CategoryActivity.class);
-            startActivity(intent);
-        } catch (InterruptedException e) {
-        }
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home_screen);
+		int delay = 10000; // delay for 1 sec. 
+		int period = 10000; // repeat every 10 sec. 
+		Timer timer = new Timer(); 
+		
+		timer.schedule(new TimerTask() 
+		    { 
+		        public void run() 
+		        { 
+		        	gotonext();  // display the data
+		        } 
+		    }, delay); 
+		
+	}
+
+	private void gotonext() {
+		// TODO Auto-generated method stub
+		
+		Intent intent = new Intent(this, CategoryActivity.class);
+		startActivity(intent);              
+	
+	}
 }
