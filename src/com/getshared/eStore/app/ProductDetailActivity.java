f53424a25e35305d3f5772962bc4aa51f9db7438
davidson <davidson0790@gmail.com>
2015-12-11 18:41:22 +0530

    Added Default Images In Products

--- src/com/getshared/eStore/app/ProductDetailActivity.java
+++ src/com/getshared/eStore/app/ProductDetailActivity.java
@@ -2,22 +2,25 @@ package com.getshared.eStore.app;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.squareup.picasso.Picasso;
 
 public class ProductDetailActivity extends Activity {
+	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.gallery);
+		
 		Intent intent = getIntent();
-		final Bitmap bitmap = (Bitmap) intent.getParcelableExtra("bitmapImage1");
-		final Bitmap bitmap2 = (Bitmap) intent.getParcelableExtra("bitmapImage2");
+		/*final Bitmap bitmap = (Bitmap) intent.getParcelableExtra("bitmapImage1");
+		final Bitmap bitmap2 = (Bitmap) intent.getParcelableExtra("bitmapImage2");*/
 		String name = intent.getStringExtra("name");
+		String image = intent.getStringExtra("bitmapImage3");
 		String price = intent.getStringExtra("price");
 		String rupeeSymbol = getResources().getString(R.string.rs);
 		final String Link = intent.getStringExtra("link");
@@ -31,10 +34,16 @@ public class ProductDetailActivity extends Activity {
 
 		TextView productInfo = (TextView) findViewById(R.id.productdetail_info);
 		productInfo.setText(productInf);
-
-		final ImageView productDetailImageView = (ImageView) findViewById(R.id.productdetail_image);
-		productDetailImageView.setImageBitmap(bitmap);
-		final ImageView second_button = (ImageView) findViewById(R.id.test_photo2);
+ 
+		  ImageView productDetailImageView = (ImageView) findViewById(R.id.productdetail_image);
+		 if(image != null){
+		 Picasso.with(ProductDetailActivity.this).load(image).into(productDetailImageView);
+		 }
+		 else
+		 {
+			 Picasso.with(ProductDetailActivity.this).load(R.drawable.cartdefault).into(productDetailImageView); 
+		 }
+		/*final ImageView second_button = (ImageView) findViewById(R.id.test_photo2);
 		second_button.setImageBitmap(bitmap);
 		if (bitmap2 != null) {
 			final ImageView third_button = (ImageView) findViewById(R.id.test_photo3);
@@ -44,7 +53,7 @@ public class ProductDetailActivity extends Activity {
 					productDetailImageView.setImageBitmap(bitmap2);
 				}
 			});
-		} else {
+			} else {
 			final ImageView third_button = (ImageView) findViewById(R.id.test_photo3);
 			third_button.setVisibility(View.INVISIBLE);
 		}
@@ -52,7 +61,7 @@ public class ProductDetailActivity extends Activity {
 			public void onClick(View v) {
 				productDetailImageView.setImageBitmap(bitmap);
 			}
-		});
+		});*/
 
 		TextView newPage1 = (TextView) findViewById(R.id._business);
 		newPage1.setOnClickListener(new View.OnClickListener() {
